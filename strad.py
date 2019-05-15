a157b3f4b376501b6ed49c65b9b83595acb46d6b
Nico Verrijdt <nvrrdt@gmail.com>
2019-05-15 21:33:49 +0200

    This is the initial outline

--- strad.py
+++ strad.py
@@ -1,15 +1,36 @@
-def calc(x):
+import secrets, binascii
+
+basket = [] # a basket of hashes from transactions
+
+for i in range(10):
+    basket.append(secrets.token_hex(15)) # mockup of those hashes
+
+sum_basket = 0
+
+for i in basket:
+    sum_basket = sum_basket + int(i, 16) # sum of all the hex hashes in the basket
+
+def calc(x): # framework for finding the factors of a number
     factors = []
     i = 2
-    while i < x:
-        while x % i == 0:
-            x /= i
-            factors += [i]
-        i += 1
+    while i <= x/2:
+        if (x % i == 0):
+            factors.append(i)
+            print(factors)
+        i = i + 1
+        if (i == 10000000): # break after these cycles to move on faster
+            break
     return factors
 
-def get_large(x):
+def get_large(x): # idem framework, find largest factor
     return calc(x)[-1] #return the last factor in the list
 
+largest_factor = get_large(sum_basket)
 
-print ("The largest factor is: " +str(get_large(600851475143)))
+for i in basket:
+    if (int(i, 16) % largest_factor == 0):
+        print("ok")
+    else:
+        print("nok")
+
+# print(get_large(sum_basket))
